3d8862dcea0a6e764bbc67f3edb9d053942706e8
Carl Vincent Baydo <cvbaydo@yahoo.com.ph>
2016-11-05 03:47:37 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+
+var app = express();
+
+app.use(function (req, res, next){
+    if(req.headers['x-forwarded-proto'] === "https"){
+        res.redirect('http://' + req.hostname + req.url);
+    }else {
+        next();
+    }
+});
+
+app.use(express.static("public"));
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//    console.log("Server Started");
+// });
+
+app.listen(3000, function(){
+   console.log("Server Started");
+});
